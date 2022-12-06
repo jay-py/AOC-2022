82138654d3b39b8ae7b731e3d5141b0ae41f04da
jay_py <jeanpaull@live.it>
2022-12-06 16:52:43 +0100

    initial commit

---
+++ day1/target/debug/deps/day1-b203af95f4712e25.d
@@ -0,0 +1,5 @@
+/Users/jay_py/git/AOC-2022/day1/target/debug/deps/day1-b203af95f4712e25.rmeta: src/main.rs
+
+/Users/jay_py/git/AOC-2022/day1/target/debug/deps/day1-b203af95f4712e25.d: src/main.rs
+
+src/main.rs:

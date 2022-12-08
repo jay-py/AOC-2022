e70b33fa4721370c0c4417aad535424d920764ae
jay_py <jeanpaull@live.it>
2022-12-08 17:20:39 +0100

    add solution of day1

---
+++ day1/Solution.kt
@@ -0,0 +1,47 @@
+import java.io.File
+import java.io.InputStream
+
+fun main() {
+    part1(readInput(true))
+    part2(readInput(true))
+}
+
+fun readInput(test: Boolean = false) : String {
+    val fileName = if(test) "test.txt" else "input.txt"
+    val inputStream: InputStream = File(fileName).inputStream()
+    return inputStream.bufferedReader().use { it.readText() }
+}
+
+fun part1(input: String) {
+    val res = input.split("\n\n")
+                .map {
+                    it.split("\n")
+                        .filter {
+                            !it.isBlank()
+                        }    
+                        .map {
+                            it.toInt()
+                        }
+                        .sum()    
+                }
+                .maxOrNull()
+    println(">> part1: " + res)
+}
+
+fun part2(input: String) {
+    val res = input.split("\n\n")
+                .map {
+                    it.split("\n")
+                        .filter {
+                            !it.isBlank()
+                        }    
+                        .map {
+                            it.toInt()
+                        }
+                        .sum()    
+                }
+                .sortedDescending()
+                .take(3)
+                .sum()
+    println(">> part2: " + res)
+}
